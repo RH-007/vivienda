5698dfa832adfb5d5eb3d1dd447371551195e7dd
Renzo Huertas <renzo.huertas007@gmail.com>
2025-09-07 14:57:05 -0500

    añadir images en el top

--- adonde_vivir_oficial.py
+++ adonde_vivir_oficial.py
@@ -14,7 +14,29 @@ from urllib.parse import quote
 import plotly.express as px
 
 
+st.set_page_config(layout="wide")
+# Custom HTML/CSS for the banner
+custom_html = """
+<div class="banner">
+    <img src="https://img.freepik.com/premium-photo/wide-banner-with-many-random-square-hexagons-charcoal-dark-black-color_105589-1820.jpg" alt="Banner Image">
+</div>
+<style>
+    .banner {
+        width: 160%;
+        height: 200px;
+        overflow: hidden;
+    }
+    .banner img {
+        width: 100%;
+        object-fit: cover;
+    }
+</style>
+"""
+# Display the custom HTML
+st.components.v1.html(custom_html)
+
 ## Titulo
+# st.image("sunrise.jpg", caption="Sunrise by the mountains")
 st.set_page_config(layout="wide")
 st.title("Análisis Inmobiliario 🏡📊")
 
